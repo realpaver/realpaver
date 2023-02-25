041ff53dbe2fed9a283be465f6316cd64887d3b6
granvilliers-l <laurent.granvilliers@univ-nantes.fr>
2023-02-25 11:26:13 +0100

    Refactor real functions

--- src/realpaver/RealFunction.cpp
+++ src/realpaver/RealFunction.cpp
@@ -12,57 +12,156 @@
 
 namespace realpaver {
 
+RealFunctionRep::~RealFunctionRep()
+{}
+
+void RealFunctionRep::setImage(const Interval& img)
+{
+   img_ = img;
+}
+
+Interval RealFunctionRep::getImage() const
+{
+   return img_;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
 RealFunction::RealFunction(SharedDag dag, size_t i)
-      : dag_(dag),
-        index_(i),
-        val_()
+      : rep_(std::make_shared<RealFunctionDag>(dag, i))
+{}
+
+RealFunction::RealFunction(Term t, const Interval& img)
+      : rep_(std::make_shared<RealFunctionDag>(t, img))
+{}
+
+RealFunction::RealFunction(SharedRep rep)
+      : rep_(rep)
+{
+   ASSERT(rep != nullptr,
+          "Creation of an interval function from a null pointer");
+}
+
+RealFunction::SharedRep RealFunction::rep() const
+{
+   return rep_;
+}
+
+void RealFunction::setImage(const Interval& img)
+{
+   rep_->setImage(img);
+}
+
+Interval RealFunction::getImage() const
+{
+   return rep_->getImage();
+}
+
+Scope RealFunction::scope() const
+{
+   return rep_->scope();
+}
+
+size_t RealFunction::nbVars() const
+{
+   return rep_->nbVars();
+}
+
+double RealFunction::eval(const RealPoint& pt)
+{
+   return rep_->eval(pt);
+}
+
+void RealFunction::diff(const RealPoint& pt, RealVector& grad)
+{
+   return rep_->diff(pt, grad);
+}
+
+void RealFunction::evalDiff(const RealPoint& pt, double& val, RealVector& grad)
+{
+   return rep_->evalDiff(pt, val, grad);
+}
+
+void RealFunction::violation(const RealPoint& pt, double& val, double& viol)
+{
+   rep_->violation(pt, val, viol);
+}
+
+void RealFunction::violation(const RealPoint& pt, double lo, double up, double& val,
+                             double& viol)
+{
+   rep_->violation(pt, lo, up, val, viol);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+RealFunctionDag::RealFunctionDag(SharedDag dag, size_t i)
+      : RealFunctionRep(),
+        dag_(dag),
+        index_(i)
 {
    ASSERT(dag_ != nullptr, "Null pointer used to create a real function");
    ASSERT(i < dag_->nbFuns(), "Bad index used to create a real function");
 }
 
-RealFunction::~RealFunction()
-{}
+RealFunctionDag::RealFunctionDag(Term t, const Interval& img)
+      : dag_(nullptr),
+        index_(0)
+{
+   dag_ = std::make_shared<Dag>();
+   index_ = dag_->insert(t, img);
+}
 
-SharedDag RealFunction::dag() const
+SharedDag RealFunctionDag::dag() const
 {
    return dag_;
 }
 
-Scope RealFunction::scope() const
+size_t RealFunctionDag::index() const
+{
+   return index_;
+}
+
+Scope RealFunctionDag::scope() const
 {
    return dag_->fun(index_)->scope();
 }
 
-size_t RealFunction::nbVars() const
+size_t RealFunctionDag::nbVars() const
 {
    return dag_->fun(index_)->nbVars();   
 }
 
-Interval RealFunction::getImage() const
+double RealFunctionDag::eval(const RealPoint& pt)
 {
-   return dag_->fun(index_)->getImage();   
+   return dag_->fun(index_)->realEval(pt);
 }
 
-double RealFunction::getValue() const
+void RealFunctionDag::diff(const RealPoint& pt, RealVector& grad)
 {
-   return val_;
+   double val;
+   evalDiff(pt, val, grad);
 }
 
-double RealFunction::eval(const RealPoint& pt)
+void RealFunctionDag::evalDiff(const RealPoint& pt, double& val,
+                               RealVector& grad)
 {
-   val_ = dag_->fun(index_)->realEval(pt);
-   return val_;
+   ASSERT(nbVars() == grad.size(), "Bad size of gradient");
+
+   DagFun* f = dag_->fun(index_);
+   val = f->realEval(pt);
+   f->realDiff(grad);
 }
 
-double RealFunction::violation(const RealPoint& pt)
+void RealFunctionDag::violation(const RealPoint& pt, double& val, double& viol)
 {
    DagFun* f = dag_->fun(index_);
-   val_ = f->realEval(pt);
-   return f->realViolation();
+   val = f->realEval(pt);
+   viol = f->realViolation();   
 }
 
-double RealFunction::violation(const RealPoint& pt, double lo, double up)
+void RealFunctionDag::violation(const RealPoint& pt, double lo, double up,
+                                double& val, double& viol)
 {
    Interval img(lo, up);
    ASSERT(!img.isEmpty(), "Empty image for a real function");
@@ -71,20 +170,10 @@ double RealFunction::violation(const RealPoint& pt, double lo, double up)
    Interval tmp = f->getImage();
    f->setImage(img);
 
-   val_ = f->realEval(pt);
-   double v = f->realViolation();
+   val = f->realEval(pt);
+   viol = f->realViolation();
 
    f->setImage(tmp);
-   return v;
-}
-
-void RealFunction::diff(const RealPoint& pt, RealVector& grad)
-{
-   ASSERT(nbVars() == grad.size(), "Bad size of gradient");
-
-   DagFun* f = dag_->fun(index_);
-   val_ = f->realEval(pt);
-   f->realDiff(grad);
 }
 
 } // namespace
--- src/realpaver/RealFunction.hpp
+++ src/realpaver/RealFunction.hpp
@@ -15,28 +15,114 @@
 namespace realpaver {
 
 ///////////////////////////////////////////////////////////////////////////////
-/// This is a real-valued function.
+/// This is the base class of the hierarchy of representations of real
+/// functions.
+///////////////////////////////////////////////////////////////////////////////
+class RealFunctionRep {
+public:
+   /// Default constructor
+   RealFunctionRep() = default;
+
+   /// Default copy constructor
+   RealFunctionRep(const RealFunctionRep&) = default;
+
+   /// No assignment
+   RealFunctionRep& operator=(const RealFunctionRep&) = delete;
+
+   /// Virtual destructor
+   virtual ~RealFunctionRep();
+
+   /// Assigns the image of this
+   void setImage(const Interval& img);
+
+   /// @return the image of this
+   Interval getImage() const;
+
+   /// @return the scope of this, i.e. the set of variables
+   virtual Scope scope() const = 0;
+
+   /// @return the number of arguments of this
+   virtual size_t nbVars() const = 0;
+
+   /// Evaluates this
+   /// @param pt values of variables
+   /// @return value of this at pt
+   virtual double eval(const RealPoint& pt) = 0;
+
+   /// Differentiates this
+   /// @param pt values of variables
+   /// @param grad output vector such that grad[i] if the derivative of this
+   /// at pt with respect to the i-th variable of its scope
+   virtual void diff(const RealPoint& pt, RealVector& grad) = 0;
+
+   /// Evaluates and differentiates this
+   /// @param pt values of variables
+   /// @param val result of evaluation of this at pt
+   /// @param grad output vector such that grad[i] if the derivative of this
+   /// at pt with respect to the i-th variable of its scope
+   virtual void evalDiff(const RealPoint& pt, double& val,
+                         RealVector& grad) = 0;
+
+   /// Evaluates this and calculates the violation of the underlying constraint
+   /// @param pt values of variables
+   /// @param val evaluation of this at reg
+   /// @param viol 0.0 if the constraint is satisfied, a positive real number
+   ///        otherwise equal to the width of the gap between the image of the
+   ///        function and the result of its evaluation at pt
+   ///
+   /// Given [lo, up] the image of this in the DagFun object, the underlying
+   /// constraint is defined by lo <= f(x) <= up.
+   virtual void violation(const RealPoint& pt, double& val, double& viol) = 0;
+
+   /// Evaluates this and calculates the violation of the underlying constraint
+   /// @param pt values of variables
+   /// @param lo left bound for this
+   /// @param up right bound for this
+   /// @param val evaluation of this at pt
+   /// @param viol 0.0 if the constraint is satisfied, a positive real number
+   ///        otherwise equal to the width of the gap between the image of the
+   ///        function and the result of its evaluation at pt
+   ///
+   /// The underlying constraint is defined by lo <= f(x) <= up.
+   virtual void violation(const RealPoint& pt, double lo, double up,
+                          double& val, double& viol) = 0;
+
+private:
+   Interval img_;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+/// This is the main class of real functions.
 ///
-/// This class is an adapter of the DagFun class.
+/// This encloses a shared pointer to its representation. It is a lightweight
+/// object that can be copied and assigned.
 ///////////////////////////////////////////////////////////////////////////////
 class RealFunction {
 public:
-   /// Constructor
+   /// Constructor given an existing dag
    /// @param dag expression graph
    /// @param i index of function in the DAG
    RealFunction(SharedDag dag, size_t i);
 
-   /// Virtual destructor
-   virtual ~RealFunction();
+   /// Constructor that creates a DAG from a term
+   /// @param t a trerm
+   /// @param img the image of t
+   RealFunction(Term t, const Interval& img = Interval::universe());
 
-   /// Copy constructor
+   /// Default destructor
+   ~RealFunction() = default;
+
+   /// Default copy constructor
    RealFunction(const RealFunction&) = default;
 
-   /// No asssignment
+   /// No assignment
    RealFunction& operator=(const RealFunction&) = delete;
 
-   /// @return the dag
-   SharedDag dag() const;
+   /// Assigns the image of this
+   void setImage(const Interval& img);
+
+   /// @return the image of this
+   Interval getImage() const;
 
    /// @return the scope of this, i.e. the set of variables
    Scope scope() const;
@@ -44,54 +130,108 @@ public:
    /// @return the number of arguments of this
    size_t nbVars() const;
 
-   /// @return the image of this
-   Interval getImage() const;
-
-   /// @return the value obtained from the last evaluation
-   double getValue() const;
-
    /// Evaluates this
    /// @param pt values of variables
    /// @return value of this at pt
    double eval(const RealPoint& pt);
 
-   /// Evaluates and differentiates this
+   /// Differentiates this
    /// @param pt values of variables
    /// @param grad output vector such that grad[i] if the derivative of this
    /// at pt with respect to the i-th variable of its scope
-   ///
-   /// getValue() returns the value of this if needed
    void diff(const RealPoint& pt, RealVector& grad);
 
+   /// Evaluates and differentiates this
+   /// @param pt values of variables
+   /// @param val result of evaluation of this at pt
+   /// @param grad output vector such that grad[i] if the derivative of this
+   /// at pt with respect to the i-th variable of its scope
+   void evalDiff(const RealPoint& pt, double& val, RealVector& grad);
+
    /// Evaluates this and calculates the violation of the underlying constraint
-   /// @param pt the values of the variables
-   /// @return 0.0 if the constraint is satisfied, a positive real number
-   ///         otherwise equal to the width of the gap between the image of the
-   ///         function and the result of its evaluation at pt
+   /// @param pt values of variables
+   /// @param val evaluation of this at reg
+   /// @param viol 0.0 if the constraint is satisfied, a positive real number
+   ///        otherwise equal to the width of the gap between the image of the
+   ///        function and the result of its evaluation at pt
    ///
    /// Given [lo, up] the image of this in the DagFun object, the underlying
    /// constraint is defined by lo <= f(x) <= up.
-   ///
-   /// getValue() returns the value of this if needed
-   double violation(const RealPoint& pt);
+   void violation(const RealPoint& pt, double& val, double& viol);
 
    /// Evaluates this and calculates the violation of the underlying constraint
-   /// @param pt the values of the variables
+   /// @param pt values of variables
    /// @param lo left bound for this
    /// @param up right bound for this
-   /// @return 0.0 if the constraint is satisfied, a positive real number
-   ///         otherwise equal to the width of the gap between the image of the
-   ///         function and the result of its evaluation at pt
+   /// @param val evaluation of this at pt
+   /// @param viol 0.0 if the constraint is satisfied, a positive real number
+   ///        otherwise equal to the width of the gap between the image of the
+   ///        function and the result of its evaluation at pt
    ///
    /// The underlying constraint is defined by lo <= f(x) <= up.
-   ///
-   /// getValue() returns the value of this if needed
-   double violation(const RealPoint& pt, double lo, double up);
+   void violation(const RealPoint& pt, double lo, double up, double& val,
+                  double& viol);
+
+   /// type of shared pointer to a representation
+   typedef std::shared_ptr<RealFunctionRep> SharedRep;
+
+   /// Constructor
+   /// @param rep representation of this
+   RealFunction(SharedRep rep);
+
+   /// @return the representation of this
+   SharedRep rep() const;
 
 private:
-   SharedDag dag_;      // DAG
-   size_t index_;       // index of function
-   double val_;         // value
+   SharedRep rep_;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+/// This is a real-valued function in a DAG.
+///
+/// This class is an adapter of the DagFun class.
+///////////////////////////////////////////////////////////////////////////////
+class RealFunctionDag : public RealFunctionRep {
+public:
+   /// Constructor given an existing dag
+   /// @param dag expression graph
+   /// @param i index of function in the DAG
+   RealFunctionDag(SharedDag dag, size_t i);
+
+   /// Constructor that creates a DAG from a term
+   /// @param t a trerm
+   /// @param img the image of t
+   RealFunctionDag(Term t, const Interval& img = Interval::universe());
+
+   /// Default destructor
+   ~RealFunctionDag() = default;
+
+   /// Copy constructor
+   RealFunctionDag(const RealFunctionDag&) = default;
+
+   /// No asssignment
+   RealFunctionDag& operator=(const RealFunctionDag&) = delete;
+
+   /// @return the dag
+   SharedDag dag() const;
+
+   /// @return the function index in the dag
+   size_t index() const;
+
+   ///@{
+   Scope scope() const override;
+   size_t nbVars() const override;
+   double eval(const RealPoint& pt) override;
+   void diff(const RealPoint& pt, RealVector& grad) override;
+   void evalDiff(const RealPoint& pt, double& val, RealVector& grad) override;
+   void violation(const RealPoint& pt, double& val, double& viol) override;
+   void violation(const RealPoint& pt, double lo, double up, double& val,
+                  double& viol) override;
+   ///@}
+
+private:
+   SharedDag dag_;         // DAG
+   size_t index_;          // index of function
 };
 
 } // namespace
