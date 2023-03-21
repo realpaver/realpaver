701c056c2b21fd608c56bc3692e15b06f4f323a2
granvilliers-l <laurent.granvilliers@univ-nantes.fr>
2023-03-21 08:36:59 +0100

    Improve the output format of solutions

--- examples/ncsp/ncsp_solver.cpp
+++ examples/ncsp/ncsp_solver.cpp
@@ -13,12 +13,14 @@
 using namespace realpaver;
 using namespace std;
 
-#define BLACK(s) "\033[30m" << s << "\033[39m"
-#define RED(s)   "\033[31m" << s << "\033[39m"
-#define BLUE(s)  "\033[34m" << s << "\033[39m"
-#define GRAY(s)  "\033[37m" << s << "\033[39m"
-#define GREEN(s) "\033[32m" << s << "\033[39m"
-#define ORANGE(s) "\033[33m" << s << "\033[39m"
+#define BLACK(s)   "\033[30m" << s << "\033[39m"
+#define RED(s)     "\033[31m" << s << "\033[39m"
+#define GREEN(s)   "\033[32m" << s << "\033[39m"
+#define ORANGE(s)  "\033[33m" << s << "\033[39m"
+#define BLUE(s)    "\033[34m" << s << "\033[39m"
+#define MAGENTA(s) "\033[35m" << s << "\033[39m"
+#define CYAN(s)    "\033[36m" << s << "\033[39m"
+#define GRAY(s)    "\033[37m" << s << "\033[39m"
 
 // returns the filename without its extension
 string baseFilename(const std::string& filename);
@@ -99,15 +101,15 @@ int main(int argc, char** argv)
       cout << GRAY(sep) << endl;
       cout << BLUE("Files") << endl;
 
-      cout << indent << WP("Input file", wpl) << ORANGE(filename) << endl;
+      cout << indent << WP("Input file", wpl) << BLACK(filename) << endl;
 
 #if LOG_ON
       cout << indent << WP("Log file", wpl);
       string meslog = (loglevel != LogLevel::none) ? flog : "no log";
-      cout << ORANGE(meslog) << endl;
+      cout << BLACK(meslog) << endl;
 #endif
 
-      cout << indent << WP("Output file", wpl) << ORANGE(solfilename) << endl;
+      cout << indent << WP("Output file", wpl) << BLACK(solfilename) << endl;
       cout << GRAY(sep) << endl;
 
       // preprocessing
@@ -126,7 +128,7 @@ int main(int argc, char** argv)
       if (prepro == "YES")
       {
          fsol << "--- PREPROCESSING ---" << endl << endl
-              << std::fixed << std::setprecision(2)
+              << std::fixed << std::setprecision(3)
               << WP("Elapsed time", wpl)
               << preproc->elapsedTime() << " (seconds)"
               << endl
@@ -134,9 +136,9 @@ int main(int argc, char** argv)
               << WP("Status", wpl);
 
          cout << BLUE("Preprocessing") << endl;
-         cout << std::fixed << std::setprecision(2)
+         cout << std::fixed << std::setprecision(3)
               << indent << WP("Time", wpl)
-              << ORANGE(preproc->elapsedTime() << "s")
+              << GREEN(preproc->elapsedTime() << " (seconds)")
               << endl
               << std::fixed << std::setprecision(2)
               << indent << WP("Status", wpl);
@@ -146,72 +148,78 @@ int main(int argc, char** argv)
             if (preproc->isUnfeasible())
             {
                fsol << "solved unfeasible" << endl;
-               cout << ORANGE("solved unfeasible") << endl;
+               cout << RED("solved unfeasible") << endl;
             }
             else
             {
                fsol << "solved feasible" << endl;
-               cout << ORANGE("solved feasible") << endl;
+               cout << GREEN("solved feasible") << endl;
+
+               IntervalRegion reg(preproc->fixedRegion());
+               fsol << endl << "SOLUTION " << std::scientific
+                    << "[" << reg.width() << "]"
+                    << endl << reg;
             }
          }
          else
          {
             fsol << "not solved" << endl;
-            cout << ORANGE("not solved") << endl;
-         }
+            cout << RED("not solved") << endl;
 
-         fsol << WP("Number of variables fixed", wpl)
-              << preproc->nbFixedVars() << endl;
+            fsol << WP("Number of variables fixed", wpl)
+                 << preproc->nbFixedVars() << endl;
 
-         cout << indent << WP("Number of variables fixed", wpl)
-              << ORANGE(preproc->nbFixedVars()) << endl
-              << indent << WP("Number of inactive constraints", wpl)
-              << ORANGE(preproc->nbInactiveCtrs()) << endl;
+            cout << indent << WP("Number of variables fixed", wpl)
+                 << GREEN(preproc->nbFixedVars()) << endl
+                 << indent << WP("Number of inactive constraints", wpl)
+                 << GREEN(preproc->nbInactiveCtrs()) << endl;
 
-         cout << GRAY(sep) << endl;
-
-         if (preproc->nbFixedVars() > 0)
-         {
-            IntervalRegion reg(preproc->fixedRegion());
-            Scope sco = preproc->fixedScope();
-            reg.stdPrint(fsol);
-         }
-
-         fsol << WP("Number of inactive constraints", wpl)
-              << preproc->nbInactiveCtrs() << endl;
-
-         if (preproc->nbInactiveCtrs() > 0)
-         {
-            for (size_t i=0; i<preproc->nbInactiveCtrs(); ++i)
+            if (preproc->nbFixedVars() > 0)
             {
-               fsol << preproc->getInactiveCtr(i) << endl;
+               IntervalRegion reg(preproc->fixedRegion());
+               Scope sco = preproc->fixedScope();
+               reg.stdPrint(fsol);
+            }
+
+            fsol << WP("Number of inactive constraints", wpl)
+                 << preproc->nbInactiveCtrs() << endl;
+
+            if (preproc->nbInactiveCtrs() > 0)
+            {
+               for (size_t i=0; i<preproc->nbInactiveCtrs(); ++i)
+               {
+                  fsol << preproc->getInactiveCtr(i) << endl;
+               }
             }
             fsol << endl;
          }
       }
 
+      cout << GRAY(sep) << endl;
+
+      NcspEnv* env = solver.getEnv();
+
       // solving
       if (!(prepro == "YES" && preproc->isSolved()))
       {
-         NcspEnv* env = solver.getEnv();
          NcspSpace* space = solver.getSpace();
 
          fsol << "--- SOLVING ---" << endl << endl;
          cout << BLUE("Solving") << endl;
 
          fsol << WP("Elapsed time", wpl)
-              << std::fixed << std::setprecision(2)
+              << std::fixed << std::setprecision(3)
               << solver.getSolvingTime() << " (seconds)"
               << endl
               << WP("Number of nodes", wpl)
               << solver.getTotalNodes() << endl;
 
          cout << indent << WP("Time", wpl)
-              << std::fixed << std::setprecision(2)
-              << ORANGE(solver.getSolvingTime() << "s")
+              << std::fixed << std::setprecision(3)
+              << GREEN(solver.getSolvingTime() << " (seconds)")
               << endl
               << indent << WP("Number of nodes", wpl)
-              << ORANGE(solver.getTotalNodes()) << endl;
+              << GREEN(solver.getTotalNodes()) << endl;
 
          bool complete = env->usedNoLimit() &&
                          space->nbPendingNodes() == 0;
@@ -221,12 +229,12 @@ int main(int argc, char** argv)
          if (complete)
          {
             fsol << "complete" << endl;
-            cout << ORANGE("complete") << endl;
+            cout << GREEN("complete") << endl;
          }
          else
          {
             fsol << "partial" << endl;
-            cout << ORANGE("partial") << endl;
+            cout << RED("partial") << endl;
          }
 
          fsol << WP("Solution status", wpl);
@@ -236,12 +244,12 @@ int main(int argc, char** argv)
             if (complete)
             {
                fsol << "proved unfeasible" << endl;
-               cout << ORANGE("unfeasible") << endl;
+               cout << RED("unfeasible") << endl;
             }
             else
             {
                fsol << "no solution found" << endl;
-               cout << ORANGE("no solution found") << endl;
+               cout << RED("no solution found") << endl;
             }
          }
          else
@@ -249,111 +257,148 @@ int main(int argc, char** argv)
             if (space->hasFeasibleSolNode())
             {
                fsol << "proved feasible" << endl;
-               cout << ORANGE("feasible") << endl;
+               cout << GREEN("feasible") << endl;
             }
             else
             {
                fsol << "no proof certificate" << endl;
-               cout << ORANGE("no proof certificate") << endl;
-            }
-
-            fsol << WP("Number of clustered solutions", wpl)
-                 << space->nbSolNodes() << endl;
-            cout << indent << WP("Number of clustered solutions", wpl)
-                 << ORANGE(space->nbSolNodes()) << endl;
-
-            fsol << WP("Number of individual solutions", wpl)
-                 << space->nbTotalSolNodes() << endl;
-            cout << indent << WP("Number of individual solutions", wpl)
-                 << ORANGE(space->nbTotalSolNodes()) << endl;
-
-            if (!complete)
-            {
-               fsol << WP("Number of pending nodes", wpl)
-                    << space->nbPendingNodes() << endl;
-               cout << indent << WP("Number of pending nodes", wpl)
-                    << ORANGE(space->nbPendingNodes()) << endl;
-            }
-
-            // writes the solutions
-            Scope sco = preproc->unfixedScope();
-
-            string sdis =
-               solver.getEnv()->getParam()->getStrParam("DISPLAY_REGION");
-            if (sdis == "STD")
-            {
-               for (size_t i=0; i<space->nbSolNodes(); ++i)
-               {
-                  SharedNcspNode node = space->getSolNode(i);
-                  IntervalRegion* reg = node->region();
-                  Proof proof = node->getProof();
-
-                  fsol << std::endl << "SOLUTION " << (i+1)
-                       << " [" << reg->width() << "]";
-
-                  switch (proof)
-                  {
-                     case Proof::Inner:    fsol << " [inner]"; break;
-                     case Proof::Feasible: fsol << " [safe]"; break;
-                     case Proof::Maybe:    fsol << " [unsafe]"; break;
-                     default:              fsol << " (bug!!!)"; break;
-                  }
-
-                  fsol << endl;
-                  reg->stdPrint(fsol);                  
-               }
-            }
-            else if (sdis == "VEC")
-            {
-               fsol << endl << "SCOPE = " << sco << endl;
-               for (size_t i=0; i<space->nbSolNodes(); ++i)
-               {
-                  SharedNcspNode node = space->getSolNode(i);
-                  IntervalRegion* reg = node->region();
-                  fsol << endl;
-                  reg->vecPrint(fsol);
-               }
-            }
-            else
-            {
-               THROW("Bad parameter value: DISPLAY_REGION = " << sdis);
-            }
-
-            // writes the hull of the pending nodes
-            if (space->nbPendingNodes() > 0)
-            {
-               fsol << endl << "HULL OF PENDING NODES" << endl;
-               if (sdis == "STD")
-                  space->hullOfPendingNodes().stdPrint(fsol);
-               else if (sdis == "VEC")
-                  space->hullOfPendingNodes().vecPrint(fsol);
+               cout << RED("no proof certificate") << endl;
             }
          }
 
-         // writes the parameters in the solution file
-         fsol << endl << "--- PARAMETERS ---" << endl << endl;
-         env->getParam()->print(fsol);
+         fsol << WP("Number of clustered solutions", wpl)
+              << space->nbSolNodes() << endl;
+         fsol << WP("Number of individual solutions", wpl)
+              << space->nbTotalSolNodes() << endl;
+
+         if (space->nbSolNodes() > 0)
+         {
+            cout << indent << WP("Number of clustered solutions", wpl)
+                 << GREEN(space->nbSolNodes()) << endl;
+
+            cout << indent << WP("Number of individual solutions", wpl)
+                 << GREEN(space->nbTotalSolNodes()) << endl;
+         }
+         else
+         {
+            cout << indent << WP("Number of clustered solutions", wpl)
+                 << RED(space->nbSolNodes()) << endl;
+
+            cout << indent << WP("Number of individual solutions", wpl)
+                 << RED(space->nbTotalSolNodes()) << endl;
+         }
 
          // limits
          if (env->usedTimeLimit())
+         {
+            double tl = env->getParam()->getDblParam("TIME_LIMIT");
+            cout << std::fixed << std::setprecision(3);
+            fsol << std::fixed << std::setprecision(3);
             cout << indent << WP("Time limit enabled", wpl)
-                 << ORANGE(env->getParam()->getDblParam("TIME_LIMIT")) << endl;
-         
+                 << RED(tl << " (seconds)") << endl;
+            fsol << WP("Time limit enabled", wpl) << tl << " (seconds)" << endl;
+         }
+
          if (env->usedSolutionLimit())
+         {
+            int nb = env->getParam()->getIntParam("SOLUTION_LIMIT");
             cout << indent << WP("Solution limit enabled", wpl)
-                 << ORANGE(env->getParam()->getIntParam("SOLUTION_LIMIT"))
-                 << endl;
+                 << RED(nb) << endl;
+            fsol << WP("Solution limit enabled", wpl) << nb << endl;
+         }
 
          if (env->usedNodeLimit())
+         {
+            int nb = env->getParam()->getIntParam("NODE_LIMIT");
             cout << indent << WP("Node limit enabled", wpl)
-                 << ORANGE(env->getParam()->getIntParam("NODE_LIMIT")) << endl;
+                 << RED(nb) << endl;
+            fsol << WP("Node limit enabled", wpl) << nb << endl;
+         }
 
          if (env->usedDepthLimit())
+         {
+            int nb = env->getParam()->getIntParam("DEPTH_LIMIT");
             cout << indent << WP("Depth limit enabled", wpl)
-                 << ORANGE(env->getParam()->getIntParam("DEPTH_LIMIT")) << endl;
+                 << RED(nb) << endl;
+            fsol << WP("Depth limit enabled", wpl) << endl;
+         }
 
-         cout << GRAY(sep) << endl;
+         if (space->nbPendingNodes() > 0)
+         {
+            fsol << WP("Number of pending nodes", wpl)
+                 << space->nbPendingNodes() << endl;
+            cout << indent << WP("Number of pending nodes", wpl)
+                 << RED(space->nbPendingNodes()) << endl;
+         }
+
+         // writes the solutions
+         Scope sco = preproc->unfixedScope();
+
+         string sd =solver.getEnv()->getParam()->getStrParam("DISPLAY_REGION");
+         if (sd == "STD")
+         {
+            for (size_t i=0; i<space->nbSolNodes(); ++i)
+            {
+               fsol << std::defaultfloat << std::setprecision(4);
+
+               SharedNcspNode node = space->getSolNode(i);
+               IntervalRegion* reg = node->region();
+               Proof proof = node->getProof();
+
+               fsol << std::endl << "SOLUTION " << (i+1)
+                    << " [" << reg->width() << "]";
+
+               switch (proof)
+               {
+                  case Proof::Inner:    fsol << " [inner]"; break;
+                  case Proof::Feasible: fsol << " [safe]"; break;
+                  case Proof::Maybe:    fsol << " [unsafe]"; break;
+                  default:              fsol << " (bug!!!)"; break;
+               }
+
+               fsol << endl;
+               reg->stdPrint(fsol);                  
+            }
+         }
+         else if (sd == "VEC")
+         {
+            fsol << endl << "SCOPE = " << sco << endl;
+            for (size_t i=0; i<space->nbSolNodes(); ++i)
+            {
+               fsol << std::defaultfloat << std::setprecision(4);
+
+               SharedNcspNode node = space->getSolNode(i);
+               IntervalRegion* reg = node->region();
+               fsol << endl;
+               reg->vecPrint(fsol);
+            }
+         }
+         else
+         {
+            THROW("Bad parameter value: DISPLAY_REGION = " << sd);
+         }
+
+         // writes the hull of the pending nodes
+         if (space->nbPendingNodes() > 0)
+         {
+            IntervalRegion hpn = space->hullOfPendingNodes();
+
+            fsol << std::defaultfloat << std::setprecision(4);
+            fsol << endl << "HULL OF PENDING NODES" << " ["
+                 << hpn.width() << "]" << endl;
+            if (sd == "STD")
+               hpn.stdPrint(fsol);
+
+            else if (sd == "VEC")
+               hpn.vecPrint(fsol);
+         }
       }
+
+      // writes the parameters in the solution file
+      fsol << endl << "--- PARAMETERS ---" << endl << endl;
+      env->getParam()->print(fsol);
+
+      cout << GRAY(sep) << endl;
 
       // closes the solution file
       fsol.close();
