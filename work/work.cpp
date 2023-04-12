0b3673180fde6e0ed46026a6c99c8ad21379b997
granvilliers-l <laurent.granvilliers@univ-nantes.fr>
2023-04-12 09:03:29 +0200

    Clean up work.cpp

--- work/work.cpp
+++ work/work.cpp
@@ -2,125 +2,20 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-#include "realpaver/Dag.hpp"
-#include "realpaver/IntervalFunctionVector.hpp"
 #include "realpaver/Logger.hpp"
 #include "realpaver/NcspSolver.hpp"
-#include "realpaver/Parser.hpp"
 #include "realpaver/Problem.hpp"
-#include "realpaver/LPSolver.hpp"
-#include "realpaver/PolytopeHullContractor.hpp"
-#include "realpaver/Range.hpp"
-#include "realpaver/VariableVector.hpp"
-#include "realpaver/QuadraticTerm.hpp"
-#include "realpaver/HC4Contractor.hpp"
-#include "realpaver/RealMatrix.hpp"
-#include "realpaver/IntervalNewton.hpp"
-#include "realpaver/TermDeriver.hpp"
 
 using namespace std;
 using namespace realpaver;
-
-//~ void signal_callback_handler(int signum) {
-   //~ cout << "Caught signal " << signum << endl;
-   //~ exit(signum);
-//~ }
 
 int main(void)
 {
    Logger::init(LogLevel::full, "work.log");
    Interval::precision( 12 );
 
-   //~ signal(SIGINT, signal_callback_handler);
-   //~ while(1);
-
    try {
-      Variable x(1, 10, "x");
-      Variable y(3, 10, "y");
-
-      IntervalRegion R({x, y});
-      cout << R << endl;
-
-      Param prm;
-      Parser parser(prm);
-      Problem problem;
-
-      bool ok = parser.parseFile("../ncsp/benchmarks/TestProblem.rp", problem);
-      if (!ok) THROW("Parse error: " << parser.getParseError());
-
-      cout << problem << endl;
-
-      
-      //cout << fun->realEval(P) << endl;
-/*
-      Variable x(1, 10, "x");
-      Variable y(1, 10, "y");
-
-      IntervalFunctionVector F({sqr(x) + sqr(y) - 4, y - sqr(x)});
-      IntervalNewton N(F);
-
-      IntervalRegion R({x, y});
-      cout << R << endl;
-
-      Proof p = N.contract(R);
-      cout << p << endl << R << endl;
-*/
-
-/*
-      Interval m11(2, 3),
-               m12(-0.5, 0.7),
-               m21(-1, 2),
-               m22(-2, -1),
-               x1(-10, 10),
-               x2(-10, 10),
-               b1(3, 5),
-               b2(1, 2);
-
-      IntervalMatrix A({{m11, m12},
-                        {m21, m22}});
-      IntervalVector x({x1, x2});
-      IntervalVector b({b1, b2});
-      cout << "--- A ---" << endl << A << endl << endl;
-      cout << "--- x ---" << endl << x << endl << endl;
-      cout << "--- b ---" << endl << b << endl << endl;
-
-      IntervalGaussSeidel gs;
-      Proof p = gs.contract(A, x, b);
-      cout << "--- proof ---" << endl << p << endl << endl;
-      cout << "--- x ---" << endl << x << endl << endl;
-*/
-
-/*
-      Variable x("x");
-      Variable y("y");
-
-      IntervalRegion R({x, y});
-      R.set(x, Interval(-4, 3));
-      R.set(y, Interval(0, 7));
-      cout << R << endl;
-
-      Term ta(sqr(x) - y);
-      Term tb(x + 2*y);
-      Term tc(abs(x) - y + 1.0);
-
-      IntervalFunctionVector F({ta, tb});
-      cout << "scope : " << F.scope() << endl;
-      cout << "nb V  : " << F.nbVars() << endl;
-      cout << "nb F : " << F.nbFuns() << endl;
-
-      F.addFun(IntervalFunction(tc));
-      cout << "scope : " << F.scope() << endl;
-      cout << "nb V  : " << F.nbVars() << endl;
-      cout << "nb F : " << F.nbFuns() << endl;
-   
-      IntervalVector V(F.nbFuns());
-      F.eval(R, V);
-      cout << "eval : " << V << endl;
-
-      IntervalMatrix J(F.nbFuns(), F.nbVars());
-      F.diff(R, J);
-      cout << "diff : " << endl << J << endl;
-*/
+      // TODO
    }
    catch (Exception ex) {
       cout << ex.what() << endl;
