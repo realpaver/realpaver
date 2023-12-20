8f160877373fca7aa9611017de3f8c814cb0ddfa
granvilliers-l <laurent.granvilliers@univ-nantes.fr>
2023-12-20 11:09:34 +0100

    Implements a test for the CID contractor

---
+++ test/ctc_cid_test.cpp
@@ -0,0 +1,80 @@
+#include "test_config.hpp"
+#include "realpaver/ContractorCID.hpp"
+#include "realpaver/ContractorCtr.hpp"
+#include "realpaver/Problem.hpp"
+
+#include <memory>
+
+using namespace std;
+
+Problem* P;
+Variable x, y;
+ContractorCID* cid;
+IntervalBox* B;
+
+void init()
+{
+   P = new Problem();
+   x = P->addRealVar(0, 1, "x");
+   y = P->addRealVar(0, 1, "y");
+
+   B = new IntervalBox(P->scope());
+   Constraint c( x - y == 0 );
+
+   shared_ptr<Contractor> ctc = make_shared<ContractorCtr>(c);
+   unique_ptr<IntervalSlicer> slicer = make_unique<IntervalPartitionMaker>(3);
+   cid = new ContractorCID(ctc, x, std::move(slicer));
+}
+
+void clean()
+{
+   delete cid;
+   delete B;
+   delete P;
+}
+
+void test_1()
+{
+   B->set(x, Interval(-2, 4));
+   B->set(y, Interval(1, 2));
+
+   Proof p = cid->contract(*B);
+
+   TEST_TRUE(p == Proof::Maybe);
+   TEST_TRUE(B->get(x).isSetEq(Interval(1, 2)));
+   TEST_TRUE(B->get(y).isSetEq(Interval(1, 2)));
+}
+
+void test_2()
+{
+   B->set(x, Interval(-2, 0));
+   B->set(y, Interval(1, 2));
+
+   Proof p = cid->contract(*B);
+
+   TEST_TRUE(p == Proof::Empty);
+}
+
+void test_3()
+{
+   B->set(x, Interval(-2, 4));
+   B->set(y, Interval(-3, 2));
+
+   Proof p = cid->contract(*B);
+
+   TEST_TRUE(p == Proof::Maybe);
+   TEST_TRUE(B->get(x).isSetEq(Interval(-2, 2)));
+   TEST_TRUE(B->get(y).isSetEq(Interval(-2, 2)));
+}
+
+int main()
+{
+   INIT_TEST
+
+   TEST(test_1)
+   TEST(test_2)
+   TEST(test_3)
+
+   CLEAN_TEST
+   END_TEST
+}
