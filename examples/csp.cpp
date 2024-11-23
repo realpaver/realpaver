5076cd534ce8236199b3b24f2e905bb555fbf496
granvilliers-l <laurent.granvilliers@univ-nantes.fr>
2024-11-23 11:34:34 +0100

    Adds an example that calls the NCSP solver

---
+++ examples/csp.cpp
@@ -0,0 +1,42 @@
+#include <iomanip>
+#include <iostream>
+#include "realpaver_api.hpp"
+
+using namespace std;
+using namespace realpaver;
+
+int main(void)
+{
+   try {
+      // Defines the NCSP modelling the intersection of circles and a parabola
+      Problem problem;
+
+      Variable x = problem.addRealVar(-7, 3, "x"),
+               y = problem.addRealVar(-6, 4, "y"),
+               n = problem.addIntVar ( 0, 6, "n");
+
+      problem.addCtr({ sqr(x) + sqr(y) == sqr(n),
+                       y == sqr(x) + 1.0 });
+
+      // Solves the NCSP
+      NcspSolver solver(problem);
+
+      Timer tim;
+      tim.start();
+      solver.solve();
+      tim.stop();
+
+      // Prints the solutions
+      for (size_t i=0; i<solver.nbSolutions(); ++i) {
+         std::pair<DomainBox, Proof> sol = solver.getSolution(i);
+         cout << "Sol " << (i+1) << ": "
+              << setprecision(8) << sol.first << endl;
+      }
+      cout << "Elapsed time: " << tim.elapsedTime() << " (s)" << endl;
+   }
+   catch (Exception ex) {
+      cout << ex.what() << endl;
+   }
+
+   return 0;
+}
